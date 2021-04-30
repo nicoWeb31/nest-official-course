05fc2b2ceb1677a205514e9df8970a59f1098f4f
NicoRiot <nriot@umoove-inc.com>
2021-04-30 21:21:55 +0200

    refacto with module

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entiy';
 
 @Injectable()
@@ -17,23 +17,36 @@ export class CoffeesService {
     }
 
     findOne(id: string) {
-        return this.coffees.find((cof) => cof.id === +id);
+        const coffe = this.coffees.find((cof) => cof.id === +id);
+
+        // if (!coffe) {
+        //     throw new HttpException(
+        //         `Coffe #${id} not found`,
+        //         HttpStatus.NOT_FOUND,
+        //     );
+        // }
+
+        if( !coffe ) {
+            throw new NotFoundException(`Coffe #${id} not found`)
+        }
+
+        return coffe;
     }
 
     create(createCoffer: any) {
         this.coffees.push(createCoffer);
     }
 
-    update(id : string, updateCoffDto: any) {
-        const existingCoffe = this.findOne(id)
-        if(existingCoffe) {
+    update(id: string, updateCoffDto: any) {
+        const existingCoffe = this.findOne(id);
+        if (existingCoffe) {
             //
         }
     }
 
-    remove(id : string) {
-        const findIndex = this.coffees.findIndex(c => c.id === +id);
-        if(findIndex >= 0) {
+    remove(id: string) {
+        const findIndex = this.coffees.findIndex((c) => c.id === +id);
+        if (findIndex >= 0) {
             this.coffees.splice(findIndex, 1);
         }
     }
